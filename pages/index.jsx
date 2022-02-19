4a81c5a54694a9144377af2a9e8010797fb95c9c
Eric Chen <eric@Erics-MacBook-Air.local>
2022-02-18 22:29:06 -0800

    added metaril ui ready to go

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Typography from "@mui/material/Typography"
+
+export default function Home() {
+  return (
+    <>
+      <Typography variant="h1" component="div" gutterBottom>
+      Test Materil UI :)
+      </Typography>
+      <Typography variant="h2" component="div" gutterBottom>
+        Test Materil UI :)
+      </Typography>
+      <Typography variant="h3" component="div" gutterBottom>
+        Test Materil UI :)
+      </Typography>
+    </>
+  
+  )
+}
